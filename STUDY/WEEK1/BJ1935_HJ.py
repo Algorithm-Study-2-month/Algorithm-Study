a782722e6ce246929c81ae0ca2ba4942a4e54e36
dlaghwls1104 <43700551+dlaghwls1104@users.noreply.github.com>
2021-07-26 19:53:59 +0900

    Update BJ1935_HJ.py

--- STUDY/WEEK1/BJ1935_HJ.py
+++ STUDY/WEEK1/BJ1935_HJ.py
@@ -1,1 +1,34 @@
+priority ={
+    '*':3,
+    '/':3,
+    '+':2,
+    '-':2
+    '(':1
+}
+expr = []
+operator = Stack()
+result = []
+tmp = input()
 
+for i in tmp:
+    if i== ' ':
+        continue
+        expr.append(i)
+    
+    elif i in '+-*/':
+        if operator.is_empty():
+            operator.push(i)
+        else:
+            if priority[operator.peek()] >= priority[i]:
+                result.append(operator.pop())
+                operator.push(i)
+            else:
+                operator.push(i)
+        elif i == ')':
+            while True:
+                tmp=operator.pop()
+                if tmp == '(':
+                    break
+                result.append(tmp)
+        else:
+            result.append(i)

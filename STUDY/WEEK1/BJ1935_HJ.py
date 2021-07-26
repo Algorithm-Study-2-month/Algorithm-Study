27d66ea39133a08677fda19991e9483488fca877
dlaghwls1104 <43700551+dlaghwls1104@users.noreply.github.com>
2021-07-26 19:59:43 +0900

    Update BJ1935_HJ.py

--- STUDY/WEEK1/BJ1935_HJ.py
+++ STUDY/WEEK1/BJ1935_HJ.py
@@ -1,34 +1,19 @@
-priority ={
-    '*':3,
-    '/':3,
-    '+':2,
-    '-':2
-    '(':1
-}
-expr = []
-operator = Stack()
-result = []
-tmp = input()
-
-for i in tmp:
-    if i== ' ':
-        continue
-        expr.append(i)
-    
-    elif i in '+-*/':
-        if operator.is_empty():
-            operator.push(i)
-        else:
-            if priority[operator.peek()] >= priority[i]:
-                result.append(operator.pop())
-                operator.push(i)
-            else:
-                operator.push(i)
-        elif i == ')':
-            while True:
-                tmp=operator.pop()
-                if tmp == '(':
-                    break
-                result.append(tmp)
-        else:
-            result.append(i)
+N = int(input())
+li = list(input())
+nums = [int(input()) for _ in range(N)]
+output = []
+for t in li:
+    if t in "+-*/":
+        a = output.pop()
+        b = output.pop()
+        if t == '+':
+            output.append(b+a)
+        elif t == '-':
+            output.append(b-a)
+        elif t == '*':
+            output.append(b*a)
+        elif t == '/':
+            output.append(b/a)
+    else:
+        output.append(nums[ord(t)-ord('A')])
+print("%.2f" % (output[0]))

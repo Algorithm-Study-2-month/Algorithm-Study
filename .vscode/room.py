370b19e3efcdb2b83bfb03fc5fe03ccc0daee34d
HOJIN <dlaghwls1104@naver.com>
2021-08-09 13:37:55 +0900

    room

--- .vscode/room.py
+++ .vscode/room.py
@@ -1,16 +1,15 @@
 import heapq
-r=[]
 n = int(input())
-cnt = 1
 queue=[]
 room = [list(map(int,input().split())) for _ in range(n)]
 room.sort(key= lambda x : x[0])
+queue=[]
 heapq.heappush(queue,room[0][1])
 for i in range(1,n):
     if queue[0] > room[i][0]:
-        heapq.heappush(queue,room[0][1])
+        heapq.heappush(queue,room[i][1])
     else:
         heapq.heappop(queue)
-        heapq.heappush(queue,room[0][1])
+        heapq.heappush(queue,room[i][1])
  
 print(len(queue))
